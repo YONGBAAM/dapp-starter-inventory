2754d3393f9b943210b325eff98cb22bada2c4d6
YONGBAAM <jokester59@naver.com>
2022-03-13 14:25:04 +0900

    initial commit

---
+++ src/test/java/com/drake/dappstarterinventory/DappStarterInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drake.dappstarterinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DappStarterInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
