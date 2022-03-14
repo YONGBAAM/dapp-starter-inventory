6c888afad261bb37331ad2a5ad5a30cf48e33401
YONGBAAM <jokester59@naver.com>
2022-03-14 19:41:13 +0900

    add get item wrt account, port

--- src/main/java/com/drake/inventory/controller/ItemController.java
+++ src/main/java/com/drake/inventory/controller/ItemController.java
@@ -5,12 +5,11 @@ import com.drake.inventory.dto.RetrievalRequest;
 import com.drake.inventory.item.Item;
 import com.drake.inventory.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 @RestController
 public class ItemController {
@@ -30,5 +29,13 @@ public class ItemController {
     @GetMapping("/items")
     public List<Item> getAllItems() {
         return itemService.findAllItemList();
+    }
+
+    @GetMapping("/items/{account}")
+    public List<Item> getItemsFromAccount(@PathVariable String account) {
+
+        List<Item> allItem = itemService.findAllItemList();
+        return allItem.stream().filter(i -> i.getOwnerWalletAddress().toLowerCase(Locale.ROOT).equals(account.toLowerCase(Locale.ROOT)))
+                .collect(Collectors.toList());
     }
 }
